dda3fb8d1f7b13f6aef77b4c4205b3389945007f
Intesar Alam <alam.kir@gmail.com>
2019-02-19 21:05:46 +0530

    19022019

--- src/app/mock-heroes.ts
+++
@@ -1,9 +0,0 @@
-import {Hero} from './Hero';
-
-export const HEROES: Hero[]=[
-{ id:1,name:'Intesar Alam'},
-{ id:2,name:'Manish Kumar'},
-{ id:3,name:'Ankit Bhawal'},
-{ id:4,name:'Romana Sheikh'}
-
-];
